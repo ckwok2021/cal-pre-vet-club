bed069c6746762ba6263ed77dda70115cc76610f
mscott2757 <mscott2757@berkeley.edu>
2017-11-06 20:11:04 -0800

    added actual square officer pictures

--- src/components/Officer.js
+++ src/components/Officer.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/css/Officers.css';
 
-const squareUrl = "https://lh5.googleusercontent.com/l-0qqsMGKxtf81LgEdJBCvP3RWxBJ7RVDKhIk2usvXHdzlA0sAK2AaZDtVASTS22mZcP6byAgf1BK8hvJ2Narg_5LOXqqMcTWtyUhHZaY4i7LaVuu0Kvb9KdLEqjflaQtmfdfI7Y";
 
 class Officer extends Component {
   render() {
     return (
       <div className="officer">
         <div className={"officer__img-container officer__img-container--" + this.props.ordering} ref="imgContainer">
-          <img src={squareUrl} alt="" />
+          <img src={this.props.officer.url} alt="" />
         </div>
         <div className={"officer__body-container officer__body-container--" + this.props.ordering} ref="bodyContainer">
           <div className="body">
