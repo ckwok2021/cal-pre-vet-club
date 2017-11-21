844962d265f0c836ef19ddbb09855476886c29fc
mscott2757 <mscott2757@berkeley.edu>
2017-11-20 20:31:54 -0800

    integrating router changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Carousel from '../components/Carousel';
+import homeContent from '../content/HomeContent';
 import '../styles/css/Home.css';
 
 class Home extends Component {
@@ -10,11 +11,11 @@ class Home extends Component {
         <h2>Cal Pre-Vet Club</h2>
         <div className="home-section">
           <h3>Mission</h3>
-          <p>{this.props.content.mission}</p>
+          <p>{homeContent.mission}</p>
         </div>
         <div className="home-section">
           <h3>Why Cal PVC</h3>
-          <p>{this.props.content.whyPVC}</p>
+          <p>{homeContent.whyPVC}</p>
         </div>
       </div>
     );
