5aa5e23d5e3c7df83bacc2f9154e228076d0c888
Casey Kwok <ckwok2021@berkeley.edu>
2019-03-03 00:25:23 -0800

    App Album update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Navbar from './pages/Navbar.js';
 import Home from './pages/Home.js';
 import Gallery from './pages/Gallery.js';
+import Album2018 from './pages/albums/Album2018.js';
 import Album2017 from './pages/albums/Album2017.js';
 import Album2016 from './pages/albums/Album2016.js';
 import Album2015 from './pages/albums/Album2015.js';
 import Album2014 from './pages/albums/Album2014.js';
-import Album2013 from './pages/albums/Album2013.js';
-import Album2012 from './pages/albums/Album2012.js';
-import Album2011 from './pages/albums/Album2011.js';
+import Merchandise from './pages/albums/Merchandise.js';
 import Members from './pages/Members.js';
 import Officers from './pages/Officers.js';
 import Contact from './pages/Contact.js';
@@ -23,13 +22,13 @@ const Main = () => (
       <Route exact path='/members' component={Members}/>
       <Route exact path='/officers' component={Officers}/>
       <Route exact path='/contact' component={Contact}/>
+      <Route exact path='/photos/2018-2019' component={Album2018}/>
       <Route exact path='/photos/2017-2018' component={Album2017}/>
       <Route exact path='/photos/2016-2017' component={Album2016}/>
       <Route exact path='/photos/2015-2016' component={Album2015}/>
       <Route exact path='/photos/2014-2015' component={Album2014}/>
-      <Route exact path='/photos/2013-2014' component={Album2013}/>
-      <Route exact path='/photos/2012-2013' component={Album2012}/>
-      <Route exact path='/photos/2011-2012' component={Album2011}/>
+      <Route exact path='/photos/Merchandise' component={Merchandise}/>
+      
     </Switch>
   </main>
 );
